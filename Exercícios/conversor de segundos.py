6704ea055ee0d1df9939f6e13fac91b10af982a2
Vitor Hugo <29439693+vitorhg@users.noreply.github.com>
2020-03-31 17:16:51 -0300

    Update conversor de segundos.py

--- Exercícios/conversor de segundos.py
+++ Exercícios/conversor de segundos.py
@@ -9,4 +9,11 @@ minutosrestantes = minutos % 60
 dias = horas // 24
 horasrestantes = horas % 24
 
-print(f"{dias} dias, {horasrestantes} horas, {minutosrestantes} minutos e {segundosrestantes} segundos.")
+print(f"{dias} dias, {horasrestantes} horas, {minutosrestantes} minutos e {segundosrestantes} segundos.")anos = dias // 365
+diascalculo = dias % 365
+
+meses = diascalculo // 30
+diasrestantes = diascalculo % 30
+
+print(f"{segundos} segundos são equivalentes à: {anos} anos, {meses} meses, {diasrestantes} dias, {horasrestantes} horas, {minutosrestantes} minutos e {segundosrestantes} segundos.")
+
