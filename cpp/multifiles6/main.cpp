82eed4253abd6cd430464be355cc309669e9a22b
Vitor Hugo Cândido Camargo <29439693+vitorhg@users.noreply.github.com>
2020-09-25 19:56:47 -0300

    exercícios

---
+++ cpp/multifiles6/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "numbers.hpp"
+
+int main() {
+  
+  std::cout << get_smallest(100, 60) << "\n";
+  std::cout << get_smallest(2543.2, 3254.3) << "\n";
+  
+}
+
+//g++ main.cpp numbers.cpp
---
+++ cpp/multifiles6/numbers.cpp
@@ -0,0 +1,5 @@
+
+// Replace the function declarations in numbers.hpp with a function template that allows for 
+// various number types and returns the type passed in. (For example, if you pass in two ints, 
+// the function should return an int.) 
+// Then remove the definitions in numbers.cpp (they are now unnecessary) and run main.cpp.
