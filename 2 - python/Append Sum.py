84b26bb3c823d4406bfa1e1b583a6c4f48f54533
Vitor Hugo Cândido Camargo <29439693+vitorhg@users.noreply.github.com>
2023-03-14 18:15:50 -0300

    comentários e documentação

--- 2 - python/Append Sum.py
+++ 2 - python/Append Sum.py
@@ -1,14 +1,30 @@
 def append_sum(lst):
+    """
+    Esta função adiciona três novos elementos à lista dada como entrada, calculando a soma dos dois últimos elementos da lista e adicionando esse resultado ao final da lista. O processo é repetido duas vezes mais.
+
+    Parâmetros:
+    lst (list): Lista a ser modificada.
+
+    Retorna:
+    list: Lista modificada com três novos elementos adicionados.
+
+    Exemplo:
+    >>> append_sum([1, 1, 2])
+    [1, 1, 2, 2, 3, 5]
+    """
+    # calcula a soma dos dois últimos elementos da lista
     soma = lst[-2] + lst[-1]
+    # adiciona a soma ao final da lista
     lst.append(soma)
+    # calcula a soma dos dois últimos elementos da lista novamente
     soma1 = lst[-2] + lst[-1]
+    # adiciona a soma ao final da lista novamente
     lst.append(soma1)
+    # calcula a soma dos dois últimos elementos da lista novamente
     soma2 = lst[-2] + lst[-1]
+    # adiciona a soma ao final da lista novamente
     lst.append(soma2)
+    # retorna a lista modificada com os novos elementos adicionados
     return lst
-
-
-# The function should add the last two elements of lst together and append the result to lst.
-# It should do this process three times and then return lst.
 
 print(append_sum([1, 1, 2]))
